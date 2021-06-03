8c1055fff43e8e18756c6ae6d0a986e19ce5a36d
Richard Hawkins <hawkinsr1991@gmail.com>
2021-06-03 15:35:48 -0400

    first commit

---
+++ phone.py
@@ -0,0 +1,52 @@
+import unittest
+
+# write a method that takes in a a phone_number as a string
+# if the string contains letters it is invalid
+# if a strig does not have 10 digits it is invalid
+# any other chracter should be ignored
+
+def isphone_number(phone_number):
+    pn = phone_number
+    pn = pn.lower()                 #convert any letters to lower case
+    if (pn.islower() == True):      #test for any lowercase, if true...
+        return False                #is not valid, return false
+    else:
+        pn = "".join(filter(str.isdigit, pn))   #else, filter phone number to numberic char
+        if len(pn) != 10:                       #if not 10 char long, false
+            return False  
+        else:
+            return True                         #if 10 char long, true
+
+
+
+########################### TESTS ##############################################################
+class TestMethods(unittest.TestCase):
+
+    def test_phone_1(self):
+        valid_number = isphone_number('555-555-5555')
+        self.assertEquals(valid_number,True)
+        
+    def test_phone_2(self):
+        valid_number = isphone_number('555!555 5555')
+        self.assertEquals(valid_number,True)
+    
+    def test_phone_3(self):
+        valid_number = isphone_number('5b5-5a5-5555')
+        self.assertEquals(valid_number,False)
+    
+    def test_phone_4(self):
+        valid_number = isphone_number('555-5a5-5555')
+        self.assertEquals(valid_number,False)
+    
+    def test_phone_5(self):
+        valid_number = isphone_number('555-555-555')
+        self.assertEquals(valid_number,False)
+
+    def test_phone_6(self):
+        valid_number = isphone_number('5555-5555-5555')
+        self.assertEquals(valid_number,False)
+
+    
+
+if __name__ == '__main__':
+    unittest.main()
